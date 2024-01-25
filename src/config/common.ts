0a77700dc9f72790180405c8edfef5c6bb290cc4
Sotatek-TuyenNguyen <tuyen.nguyen@sotatek.com>
2024-01-25 13:43:52 +0700

    add update status, time and assignee

--- src/config/common.ts
+++ src/config/common.ts
@@ -10,16 +10,20 @@ export const AssigneeId = {
   'tuan.nguyen8': '63e21185010d356379744f2f',
   'dung.du': '63e21189c2b1cb6b34738a2c',
   'huong.luong': '63e21187c2b1cb6b34738a29',
+  'hung.dinh': '712020:72d832f8-998c-44b7-a7ed-2fff9026bea6',
+  'quan.hap': '712020:867bf8ec-a6d5-4e32-8eb6-5d4544c0f906',
+  'tuyen.nguyen': '63e2118882eeee78a4d193dd',
 };
 
-export enum GoogleSheetStatus {
-  'To Do' = '10000',
-  'InProgress' = '10001',
-  'CodeReview' = '10003',
-  'Ready4Test' = '10004',
-  'DoneDev' = '10005',
-  'DoneStaging' = '10002',
-}
+export const GoogleSheetStatus = {
+  'To Do': '10000',
+  InProgress: '10001',
+  FixBugs: '10001',
+  CodeReview: '10003',
+  Ready4Test: '10004',
+  DoneDev: '10005',
+  DoneStaging: '10002',
+};
 
 export enum JiraStatus {
   'To Do' = '10000',
@@ -30,11 +34,12 @@ export enum JiraStatus {
   'DONE STAGING' = '10002',
 }
 
-export enum TransactionId {
-  'To Do' = '11',
-  'InProgress' = '21',
-  'CodeReview' = '3',
-  'Ready4Test' = '5',
-  'DoneDev' = '7',
-  'DoneStaging' = '14',
-}
+export const TransactionId = {
+  'To Do': '11',
+  InProgress: '21',
+  FixBugs: '21',
+  CodeReview: '3',
+  Ready4Test: '5',
+  DoneDev: '7',
+  DoneStaging: '14',
+};
--- src/main.ts
+++ src/main.ts
@@ -10,25 +10,40 @@ import {
   TransactionId,
 } from './config/common';
 
+function transformDate(inputDate) {
+  // Tạo một đối tượng Date hiện tại
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  // Tách ngày và tháng từ chuỗi đầu vào
+  const [day, month] = inputDate.split('/');
+
+  // Tạo chuỗi đầu ra theo định dạng 'YYYY-DD/MM'
+  const transformedDate = `${currentYear}-${month}-${day}`;
+
+  return transformedDate;
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('process.env.GOOGLE_SHEET_ID', process.env.GOOGLE_SHEET_ID);
 
   const serviceAccountAuth = new JWT({
-    email: 'helix-2@helix-390508.iam.gserviceaccount.com',
-    key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCKDzn8IxrP4CLC\nej6kzoskR8OYQR6PVID4W4a0ICL+6EztYcDdhtLfHFpwtWUnSA01tLYa2qRtvIJV\nYSPgDHf6AIBpzLP2nrEet5YhkEK/1dXh4Uz703II1M5WReQeV+JnwpUSh5Gp2LOQ\n5mXOr6pSPqq2iVuMwqRCRUxq43Hvl5MLRv+tYsAIdS098uV6GmzwfhC/PP5h5BjE\ntkeMevwzjkB8FA/Ci+pZ5sufZtPNqs8SIe80YLs0EVxTDiPBS3tTZNzDYWZjWCXQ\n5X/lGb8b+OqyfQuygNpDLPDZ/KqzhB6dNRQQnY15XCQDXJ6wIa6K7tO8ukzzZD4r\nI3mEiSK/AgMBAAECggEAP7N83sDX2zig8Tj0dJi0jN3r+vF5B0tHYtDgW3ZEj3di\nH1muYsiVvcpYVI7uCGzvY2tpZwShP9zuCjUTF4rJRPo8RhTkqJNaSEEPLZdhpSAm\nWt3Y5o0OndKLDjWbZwDH/3ZB0FXrOQI7AE2+hdp57Q7rN+Qy8MTSg510ABHH/XM2\nMb7nXkJ+Hi5qZ/yV3lnSFT0xRT9ouJBhgNbznzAH49XrqmLrsr8VQOGN3Z6g/qYe\nyF7zEN0lUFSa9WaAifGej6shr0Pug9KeJ0bgpUiqEMfgo5S6QEJDox6g71PTPrxo\nEJFoEwkGgKCRGfuug/UtS/t492Rem7+1gI/2xjBAeQKBgQC8DF1wNaUpR+W/aDLj\nS07SwDIsszpzZmPK3mGcxGsTOF88tQKCeca0CqgJjFlNtu5E4wnXkJXUBDaSxGH0\nxH9VsacwIZGyyNV/H9FiPuxwrIA6HFxsg7OR135rk78FzF7hc+qVt4bkpw0RzBi9\nvpp0BDWzMDnoAueTUFmOcoZlEwKBgQC78pXOcbBLUun9/RunKqQlyD1o6lGDUi68\nIdeSPNod/ubbEi5x/A3ooqTtNOdatzkt7VEBO3C0vBNpyF0c12B7ZbQUWk/K1gZA\n6mrzeqsMGfdpgMGk+T4IUE0AZxLTymx0pZ32Nnh8mUG0sEUaHd3f9PNy9yS77xIL\nvpOSpr09JQKBgQCELSjMQ1i5AGlTtbj3HKrl5eQemUf/bzu7j74Kw+EDXH9Mm2qN\nLW1mhYsBUx4+StOFphP36g/2O5Pka5byxd8+66U5Zo8TnKi8NzAkn1fwBsAGmIK6\nX4Qrgc3BbdM6DyGt1J1R6PMu6uB73vqQ/FVyMLdSIRbCprABUtvwDT278wKBgGri\nHBuZP962Ome8xzsb3tm1tGazysNZu0Y2ILgDby9bxMGU52DfLLG9vPlA+sj2Jche\ni5Cd0wyVYJzp2zoJqc7DGdPxQCtj546HQWdRvXBWXANdu8IsFAD6Wa/+7hAPr/nu\nLZTViVOifGFbVRDzJzizvrcEoZlW7fZL3PSrajcZAoGABlJWye+T902TWGPqeBTj\nfrW8spt8cUA+jA5bVUXib4ecZBJ6f7a2HG2w49567jkN7zCg4zJqnzvhI6Y13HNw\nuxbpH2temOIR3FGKdl0w2P9VdskeY0gfrjrrIB6ArX42ABKDFguEDKoi2KTJmvhP\njhT8iJ7y/p9e0FEIDsG5Wvk=\n-----END PRIVATE KEY-----\n',
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    email: 'update-jira@update-403109.iam.gserviceaccount.com',
+    key: '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCmN0I2NAUh5WSZ\ntci9dE3JHZGtLIREXjMIfwrYir923/tMVi0boZ5CEs749upAIfCFQQtVRe061/jy\nmrkWLMcOTI4T8/1ihTDR9gYI3ZvoKDQEaWVkKglciRfDEDrwNdRrIei/pEt2UZd+\nKJw2LZqkACCjro7ydakGoHtaoikpaq+oY2SiwVAucBF1eanBrePnBHqGshXqeGWe\nzCW1b0GMXhchyGSycbc+G9p705weARLN+6lJxLDoYnLrpfmXagQyOE2lyA1LYVJT\nOYHkyQ0rRZKuCGZgmVfgLezb6/ZerOHSPda2t7ZoSieF6MQuIcBCBMQLgqYdUp4X\nJKD1UVFPAgMBAAECggEABGJfbA/sirutmaMCi9duLdM1LSeIHZ2Lzw+QI6y/5szQ\nU2/Pz3NwjzbxH/6zUxm8vxG7e3ZVZECfdRbBBxALTW9A2CSOwciC7ybZezu33w8g\nasq05/LL6GrypiyJbB30tRa3OEbceV8m6yb7EoVwF3SsEmU0/rmSdHWgi1V3rHtJ\nrHbKeLRXnhJW24weTTxc/w7SdF3KDbDbrgCVIdd4N8NqPh8fEtMbc9mwg5hnI6Ri\nmGsf1YvPL2NFfDR6Of6OqjPo7yqq9UeeJJ725j+IxSB4zlUIcdLvzPhjkXES/jKj\naHFYuJniWWIK9QB1XxyiCelmXrY0cAPPznmvJ+tdUQKBgQDgZlYjEiDj1P1AfHNc\nKlejcZrKANQP6MjIpycTFxCXWXDtSR4P3f/zFHBuHFSyLZSAQTBZk7ZTQAAwCeV2\n34wfEiItWO1McqD5CwfUUnazbHwA7P8Nh5jRNDRDps/MYvATGWgCIG3ES7WnLyW7\nFl9OBWCHNKsbTu90IPz52Qx7TQKBgQC9n2EJKupry2Lh3zYCwLQ0snQHhe4NyMhj\nYXESp4+f8jc1RMRqdYpoNFxJ7KOIF/xIx8aiPyR4oWfCapf377qmhLQFMC8MSDpc\nzQQJzQAPAm3ckavWAXkgs0DDfnfRPI6pV3ojR89IzdYayo6OVvHDge68LlP3Rmcj\nsv86qvOZCwKBgG5zsL4+dBXwhl8xYo7hAJYgmvIHClpyWDH7pI4O8kms6prBPJzO\nxubdDqIEK+Qv70JZ0/SjckDz5aO/m0SxsASzJkNv3A2e9oDe3xGxLWAPa9IA1WHR\ndnSy327VMr8Fi+m6vakeUSknotgRqt61ml+K4gJq+DXO2qoseph4L16ZAoGAfRAZ\nF8rOQboqxX5KSRZ/xCHOlQeoMj8yqxkhUoRhHcTK1L8+LFjJqGCgXcFNOf1Xz0iz\noV6z1zvdC0TyZfwexqTPdnyJCzi4BWzSNQSX/U5E1yzcgJEIKqEPHeDTdZTTIq0n\nSQ2bkRDxHUprK0X8vk17/3Euv/AXJlxwKWmGsHECgYBwP3xJwerX83C8hOS6GjfR\nTWhYg5sDrm7kTmzRygp1nZ8EU+SH39b6T2DcuGxcRhSWv/N57n49bekTdMLU4K9K\nhVOWLAoH7h9TrEr6irgtDYS/bdasutDUMchvK0zx31c6zlFCK+ESENOpo3CDSW1A\nf6xUiiZ1AoOzJcZaEk27Tg==\n-----END PRIVATE KEY-----\n',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
   });
 
   const doc = new GoogleSpreadsheet(
     '1fChZ--blErFNixqQLlcRa3Vq5kOE_TsvO-hyeVoXaQA',
     serviceAccountAuth,
   );
+
   await doc.loadInfo();
 
-  const sheet = doc.sheetsByIndex[3];
-  const startRowIndex = 787; // Dòng bắt đầu
-  const endRowIndex = 789; // Dòng kết thúc lay den Overall Story Progress
+  const sheet = doc.sheetsById[1755250919];
+  const startRowIndex = 840; // Dòng bắt đầu
+  const endRowIndex = 956; // Dòng kết thúc lay den Overall Story Progress
   const rowStart = startRowIndex - 2;
   const rowEnd = endRowIndex - 2;
 
@@ -38,16 +53,20 @@ async function bootstrap() {
     offset: rowStart,
     limit: rowEnd - rowStart + 1,
   });
-
-  // const listTask = [];
-  const listTasks = [];
+  // let listTasks = rows;
+  const listTasksCreate = [];
   let groupTask = [];
 
   for (let i = 0; i < rows.length; i++) {
-    if (rows[i]['_rawData'][3]) {
+    if (rows[i]['_rawData'][4]) {
       groupTask.push(rows[i]);
     } else {
-      listTasks.push(groupTask);
+      const overallStory = {
+        listTasksInStory: groupTask,
+        startTime: rows[i]['_rawData'][8],
+        endTime: rows[i]['_rawData'][9],
+      };
+      listTasksCreate.push(overallStory);
       groupTask = [];
     }
   }
@@ -67,88 +86,162 @@ async function bootstrap() {
       basic: {
         email: 'tn.hl@heli-cap.com',
         apiToken:
-          'ATATT3xFfGF04frwXrvXrPqZ6DhBQPT59E3XcQLXqNYfgk4fvY8xZd-i2den6k8XqoF2CUXmLfRtToST4OPZ3tIZ5EMzk0jY7gvooVSUMWCVtvwe9xljFYraSI-6D_FKUF-Ue8Ho45Vo5iy6LCe7ByOK4Zb291fJTn6WVs3wsi34CD2EgatSZyo=2DFE066B',
+          'ATATT3xFfGF0KINBKH3fTR-m5ULJEbJ76qz0iRTM9XPRRmm19czT9RXzL4EVoXnnHItfvpwDRmSaRSq1I3oQdleSLB6biOavPC32r6Ilc4D_9gZ-ZD6al9F3lpfWhHmD6XhXhN4ifQoRuiFL6nRYLN3n9tL2KLB_2R5RFtSOyjlK6tKUZV4tXlQ=65AEA2BD',
       },
     },
   });
 
   // Tao task tren jira
-
-  for (const tasks of listTasks) {
-    // Tao story
-    const issue = {
-      fields: {
-        project: {
-          key: 'HL',
-        },
-        summary: tasks[0]['_rawData'][3],
-        issuetype: {
-          id: '10001',
-        },
-      },
-    };
-    console.log('issue', issue);
-    const epicTask = await client.issues.createIssue(issue);
-    console.log('epicTask', epicTask);
-
-    // tao sub task
-    for (const task of tasks) {
-      const issueSubTask = {
+  const createTask = async () => {
+    for (const tasks of listTasksCreate) {
+      const { listTasksInStory, startTime, endTime } = tasks;
+      console.log('listTasksInStory', listTasksInStory[0]['_rawData']);
+      // Tao story
+      const issue = {
         fields: {
           project: {
             key: 'HL',
           },
-          summary: `${task['_rawData'][3]}-${task['_rawData'][4]}`,
+          summary: `[${listTasksInStory[0]['_rawData'][1]}] ${listTasksInStory[0]['_rawData'][4]}`,
           issuetype: {
-            id: '10005',
-          },
-          parent: {
-            key: epicTask.key,
-          },
-          assignee: {
-            id: AssigneeId[task['_rawData'][12]],
+            id: '10001',
+            customfield_10015: startTime && transformDate(startTime),
+            duedate: endTime && transformDate(endTime),
           },
         },
       };
-      console.log('issueSub', issueSubTask);
-      const subTask = await client.issues.createIssue(issueSubTask);
-      console.log('subTask', subTask);
-      const link = `https://helilabs.atlassian.net/browse/${subTask.key}`;
-      const updateCell = sheet.getCellByA1(`Q${task._rowNumber}`);
-      updateCell.value = link;
-      await sheet.saveUpdatedCells();
+      console.log('issue', issue);
+      const epicTask = await client.issues.createIssue(issue);
+      console.log('epicTask', epicTask);
+
+      // tao sub task
+      for (const task of listTasksInStory) {
+        if (!task['_rawData'][17]) {
+          const issueSubTask = {
+            fields: {
+              project: {
+                key: 'HL',
+              },
+              summary: `[${task['_rawData'][4]}] ${task['_rawData'][5]}`,
+              issuetype: {
+                id: '10005',
+              },
+              parent: {
+                key: epicTask.key,
+              },
+              assignee: {
+                id: AssigneeId[task['_rawData'][13]],
+              },
+            },
+          };
+          console.log('issueSub', issueSubTask);
+          const subTask = await client.issues.createIssue(issueSubTask);
+          console.log('subTask', subTask);
+          const link = `https://helilabs.atlassian.net/browse/${subTask.key}`;
+          const updateCell = sheet.getCellByA1(`R${task._rowNumber}`);
+          updateCell.value = link;
+          await sheet.saveUpdatedCells();
+        }
+      }
     }
-  }
+  };
 
   // Update task jira
+  const updateStatusTask = async () => {
+    const listRowsUpdate = rows.filter((row) => !!row['_rawData'][17]);
 
-  // const listRowsUpdate = rows.filter((row) => !!row['_rawData'][16]);
+    for (const task of listRowsUpdate) {
+      // get status of jira
+      const key = task['_rawData'][17].split('browse/')[1];
+      console.log('task', key);
+      const issue = await client.issues.getIssue({
+        issueIdOrKey: key,
+      });
+      const status = issue.fields.status.name;
+      // const assignee = issue.fields.assignee;
+      // console.log('assignee', assignee);
+      if (
+        task['_rawData'][16] &&
+        JiraStatus[status] !== GoogleSheetStatus[task['_rawData'][16]]
+      ) {
+        const update = await client.issues.doTransition({
+          issueIdOrKey: key,
+          transition: {
+            id: TransactionId[task['_rawData'][16]],
+            to: {
+              id: GoogleSheetStatus[task['_rawData'][16]],
+            },
+          },
+        });
+        console.log(`updated task ${key}`, update);
+      }
+    }
+    console.log('updated all tasks');
+  };
 
-  // for (const task of listRowsUpdate) {
-  //   // get status of jira
-  //   const key = task['_rawData'][16].split('browse/')[1];
-  //   console.log('task', key);
-  //   const issue = await client.issues.getIssue({
-  //     issueIdOrKey: key,
-  //   });
-  //   const status = issue.fields.status.name;
-  //   if (
-  //     task['_rawData'][15] &&
-  //     JiraStatus[status] !== GoogleSheetStatus[task['_rawData'][15]]
-  //   ) {
-  //     const update = await client.issues.doTransition({
-  //       issueIdOrKey: key,
-  //       transition: {
-  //         id: TransactionId[task['_rawData'][15]],
-  //         to: {
-  //           id: GoogleSheetStatus[task['_rawData'][15]],
-  //         },
-  //       },
-  //     });
-  //     console.log(`updated task ${key}`, update);
-  //   }
-  // }
-  // console.log('updated all tasks');
+  const updateAssigneeTask = async () => {
+    const listRowsUpdate = rows.filter((row) => !!row['_rawData'][17]);
+
+    for (const task of listRowsUpdate) {
+      // get status of jira
+      const key = task['_rawData'][17].split('browse/')[1];
+      console.log('task', key);
+      const issue = await client.issues.getIssue({
+        issueIdOrKey: key,
+      });
+      const assignee = issue.fields.assignee;
+      console.log(
+        'so sanh',
+        !assignee || assignee.accountId !== AssigneeId[task['_rawData'][13]],
+      );
+      if (
+        !assignee ||
+        assignee.accountId !== AssigneeId[task['_rawData'][13]]
+      ) {
+        const update = await client.issues.assignIssue({
+          issueIdOrKey: key,
+          accountId: AssigneeId[task['_rawData'][13]],
+        });
+        console.log(`updated task ${key}`, update);
+        // console.log('update');
+      }
+    }
+    console.log('updated all tasks');
+  };
+
+  const updateTimeStory = async () => {
+    for (const tasks of listTasksCreate) {
+      const { listTasksInStory, startTime, endTime } = tasks;
+      // console.log('startTime', startTime);
+      // console.log('endTime', endTime);
+      const newIssueName = `[${listTasksInStory[0]['_rawData'][1]}] ${listTasksInStory[0]['_rawData'][4]}`;
+      console.log('newIssueName', newIssueName);
+      const key = listTasksInStory[2]['_rawData'][17].split('browse/')[1];
+      const issue = await client.issues.getIssue({
+        issueIdOrKey: key,
+      });
+      const parentKey = issue.fields.parent.key;
+      console.log('parent.key', parentKey);
+      // const parentIssue = await client.issues.getIssue({
+      //   issueIdOrKey: parentKey,
+      // });
+      const update = await client.issues.editIssue({
+        issueIdOrKey: issue.fields.parent.key,
+        fields: {
+          summary: newIssueName,
+          // customfield_10015: startTime && transformDate(startTime),
+          // duedate: endTime && transformDate(endTime),
+        },
+      });
+      console.log(`updated time story ${parentKey}`, update);
+    }
+  };
+
+  // createTask();
+  updateStatusTask();
+  // updateAssigneeTask();
+  // updateTimeStory();
+  // updateAssignee();
 
   await app.close();
 }
